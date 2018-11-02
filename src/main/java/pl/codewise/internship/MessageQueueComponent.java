3d284fcee3c4bb641a916d740cc4fc4cc412c275
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-11-02 20:25:45 +0100

    Commit

---
+++ src/main/java/pl/codewise/internship/MessageQueueComponent.java
@@ -0,0 +1,62 @@
+package pl.codewise.internship;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.stream.Collectors;
+
+public final class MessageQueueComponent implements MessageQueue {
+    private final LinkedBlockingDeque<MessageWrapper> mostRecentMessages = new LinkedBlockingDeque<>(100);
+
+    @Override
+    public void add(Message message) {
+        if(mostRecentMessages.remainingCapacity() > 0){
+            mostRecentMessages.offerFirst(new MessageWrapper(message));
+        } else {
+            mostRecentMessages.removeLast();
+            mostRecentMessages.offerFirst(new MessageWrapper(message));
+        }
+    }
+
+    @Override
+    public Snapshot snapshot() {
+        return new Snapshot(new ArrayList<>(getMessagesWithinLastFiveMinutes()));
+    }
+
+    @Override
+    public long numberOfErrorMessages() {
+        return getMessagesWithinLastFiveMinutes().stream()
+                .filter(n -> n.getErrorCode() != 0)
+                .count();
+    }
+
+    private List<Message> getMessagesWithinLastFiveMinutes(){
+        return mostRecentMessages.stream()
+                .filter(n -> isNotOlderThanFiveMinutes(n.getTimestamp()))
+                .map(MessageWrapper::getMessage)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isNotOlderThanFiveMinutes(LocalDateTime then){
+        return LocalDateTime.now().isBefore(then.plusMinutes(5));
+    }
+
+    private final class MessageWrapper{
+        private Message message;
+        private LocalDateTime timestamp;
+
+        private MessageWrapper(Message message){
+            this.message = message;
+            this.timestamp = LocalDateTime.now();
+        }
+
+        private Message getMessage(){
+            return message;
+        }
+
+        private LocalDateTime getTimestamp(){
+            return timestamp;
+        }
+    }
+}
--- src/test/java/pl/codewise/internship/MessageQueueTest.java
+++ src/test/java/pl/codewise/internship/MessageQueueTest.java
@@ -1,6 +1,31 @@
 package pl.codewise.internship;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MessageQueueTest {
-    // implement some tests
+
+    private void fillMessengerDeque(MessageQueueComponent messenger){
+        for(int i = 0; i < 150; i++) {
+            messenger.add(new Message("Test123", 0));
+        }
+    }
+
+    @Test
+    public void testAddingMessage(){
+        MessageQueueComponent messenger = new MessageQueueComponent();
+        fillMessengerDeque(messenger);
+
+        Assert.assertEquals(100, messenger.snapshot().getSnapshot().size());
+    }
+
+    @Test
+    public void testGetNumberOfErrors(){
+        MessageQueueComponent messenger = new MessageQueueComponent();
+        fillMessengerDeque(messenger);
+
+        messenger.add(new Message("321tseT", 34));
+        Assert.assertEquals(1, messenger.numberOfErrorMessages());
+    }
 }
 
