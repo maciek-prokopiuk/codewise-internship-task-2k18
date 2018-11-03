aac28c1ed8e035f935b13e6262ea29dbab518ed9
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-11-03 16:01:47 +0100

    implement ringBuffer & ReadWriteLock

--- src/main/java/pl/codewise/internship/MessageQueueComponent.java
+++ src/main/java/pl/codewise/internship/MessageQueueComponent.java
@@ -2,21 +2,19 @@ package pl.codewise.internship;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.Objects;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 
 public final class MessageQueueComponent implements MessageQueue {
-    private final LinkedBlockingDeque<MessageWrapper> mostRecentMessages = new LinkedBlockingDeque<>(100);
+    private final MessageBuffer messageBuffer = new MessageBuffer();
 
     @Override
-    public synchronized void add(Message message) {
-        if(mostRecentMessages.remainingCapacity() > 0){
-            mostRecentMessages.offerFirst(new MessageWrapper(message));
-        } else {
-            mostRecentMessages.removeLast();
-            mostRecentMessages.offerFirst(new MessageWrapper(message));
-        }
+    public void add(Message message) {
+        messageBuffer.write(new MessageWrapper(message));
     }
 
     @Override
@@ -32,7 +30,7 @@ public final class MessageQueueComponent implements MessageQueue {
     }
 
     private List<Message> getMessagesWithinLastFiveMinutes(){
-        return mostRecentMessages.stream()
+        return messageBuffer.getWrappedMessages().stream()
                 .filter(n -> isNotOlderThanFiveMinutes(n.getTimestamp()))
                 .map(MessageWrapper::getMessage)
                 .collect(Collectors.toList());
@@ -44,6 +42,39 @@ public final class MessageQueueComponent implements MessageQueue {
 
     private boolean isErrorCode(int errorCode){
         return errorCode >= 400 && errorCode < 600;
+    }
+
+    private final class MessageBuffer{
+        private final MessageWrapper[] ringBuffer;
+        private int writePosition = 0;
+        private ReadWriteLock lock = new ReentrantReadWriteLock();
+
+        private MessageBuffer(){
+            this.ringBuffer = new MessageWrapper[100];
+        }
+
+        private void write(MessageWrapper messageWrapper){
+            lock.writeLock().lock();
+            try {
+                ringBuffer[writePosition] = messageWrapper;
+                incrementWritePosition();
+            } finally {
+                lock.writeLock().unlock();
+            }
+        }
+
+        private void incrementWritePosition(){
+            writePosition = (writePosition + 1) % ringBuffer.length;
+        }
+
+        private List<MessageWrapper> getWrappedMessages(){
+            lock.readLock().lock();
+            try {
+                return Arrays.stream(ringBuffer).filter(Objects::nonNull).collect(Collectors.toList());
+            } finally {
+                lock.readLock().unlock();
+            }
+        }
     }
 
     private final class MessageWrapper{
